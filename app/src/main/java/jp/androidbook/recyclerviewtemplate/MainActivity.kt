c402f267ab15d31cc03db923b607796075622539
tanifumiya <fumiyadeveloper@gmail.com>
2020-03-24 04:20:35 +0900

    NavigationファイルをMainActivityのレイアウトに追加。

--- app/src/main/java/jp/androidbook/recyclerviewtemplate/MainActivity.kt
+++ app/src/main/java/jp/androidbook/recyclerviewtemplate/MainActivity.kt
@@ -14,42 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(toolBar)
-
-        if (savedInstanceState === null) {
-            fragmentReplace(supportFragmentManager, R.id.fragmentContainer, SimpleRecyclerFragment())
-        }
-
-        // Selected したら true を返すようにする
-        bottomNavigation.setOnNavigationItemSelectedListener {
-            transitionFragment(it.itemId)
-        }
-    }
-
-    private fun transitionFragment(itemId: Int): Boolean = when(itemId) {
-        R.id.navigation_simple -> {
-            fragmentReplace(supportFragmentManager, R.id.fragmentContainer, SimpleRecyclerFragment())
-            true
-        }
-
-        R.id.navigation_grid -> {
-            fragmentReplace(supportFragmentManager, R.id.fragmentContainer, GridSelectionFragment())
-            true
-        }
-
-        R.id.navigation_stagger_grid -> {
-            fragmentReplace(supportFragmentManager, R.id.fragmentContainer, StaggerdGridFragment())
-            true
-        }
-
-        R.id.navigation_update -> {
-            fragmentReplace(supportFragmentManager, R.id.fragmentContainer, UpdateFragment())
-            true
-        }
-
-        else -> {
-            false
-        }
     }
 }
