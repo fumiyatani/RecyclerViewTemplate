46019d938441c750d91ed8df32b77253f27a4586
tanifumiya <fumiyadeveloper@gmail.com>
2020-03-26 04:13:48 +0900

    ナビゲーションによる遷移まで行った。

--- app/src/main/java/jp/androidbook/recyclerviewtemplate/DrawerBodyItem.kt
+++ app/src/main/java/jp/androidbook/recyclerviewtemplate/DrawerBodyItem.kt
@@ -4,7 +4,7 @@ import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.android.synthetic.main.item_drawer_body.*
 
-class DrawerBodyItem(private val drawerText: String) : Item() {
+class DrawerBodyItem(private val drawerBodyId: Long, private val drawerText: String) : Item(drawerBodyId) {
     override fun getLayout() = R.layout.item_drawer_body
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
--- app/src/main/java/jp/androidbook/recyclerviewtemplate/MainActivity.kt
+++ app/src/main/java/jp/androidbook/recyclerviewtemplate/MainActivity.kt
@@ -1,8 +1,10 @@
 package jp.androidbook.recyclerviewtemplate
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,11 +12,14 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.OnItemClickListener
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +30,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupDrawerRecyclerView() {
-        val groupAdapter = GroupAdapter<GroupieViewHolder>()
+        val groupAdapter = GroupAdapter<GroupieViewHolder>().apply {
+            setOnItemClickListener { item, _ ->
+                when (item.id) {
+                    DrawerListData.DrawerBodyType.SimpleList.id -> {
+                        navController.navigate(R.id.simpleRecyclerFragment)
+                    }
+                    DrawerListData.DrawerBodyType.GridTwo.id -> {
+                        navController.navigate(R.id.gridSelectionFragment)
+                    }
+                    DrawerListData.DrawerBodyType.GridThree.id -> {
+                        navController.navigate(R.id.gridSelectionFragment)
+                    }
+                    DrawerListData.DrawerBodyType.GridStaggered.id -> {
+                        navController.navigate(R.id.staggeredGridFragment)
+                    }
+                    DrawerListData.DrawerBodyType.UpdatableList.id -> {
+                        navController.navigate(R.id.updateFragment)
+                    }
+                }
+            }
+        }
         findViewById<RecyclerView>(R.id.drawerContentRecyclerView).apply {
             layoutManager = LinearLayoutManager(this@MainActivity).apply {
                 orientation = RecyclerView.VERTICAL
@@ -36,14 +61,14 @@ class MainActivity : AppCompatActivity() {
         val items = mutableListOf<Item>(
             DrawerHeaderItem()
         )
-        for (drawerText: String in DrawerListData.getDrawerBodyList()) {
-            items.add(DrawerBodyItem(drawerText))
+        for (drawerBodyType: DrawerListData.DrawerBodyType in DrawerListData.getDrawerBodyList()) {
+            items.add(DrawerBodyItem(drawerBodyType.id, drawerBodyType.drawerBodyText))
         }
         groupAdapter.update(items)
     }
 
     private fun setupNavigation() {
-        val navController = findNavController(R.id.mainNavHostFragment)
+        navController = findNavController(R.id.mainNavHostFragment)
         val appBarConfiguration = AppBarConfiguration(navController.graph) // todo Drawer を渡す
         findViewById<Toolbar>(R.id.toolBar).apply {
             setupActionBarWithNavController(navController, appBarConfiguration)
