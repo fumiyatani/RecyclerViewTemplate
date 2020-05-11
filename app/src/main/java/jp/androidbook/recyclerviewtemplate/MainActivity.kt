6be7e83794fa84f2e3c9ab525bec401b4f92c28d
tanifumiya <fumiyadeveloper@gmail.com>
2020-05-12 02:26:11 +0900

    refactor drawer layout

--- app/src/main/java/jp/androidbook/recyclerviewtemplate/MainActivity.kt
+++ app/src/main/java/jp/androidbook/recyclerviewtemplate/MainActivity.kt
@@ -9,16 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
-import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
-import com.xwray.groupie.kotlinandroidextensions.Item
-import jp.androidbook.recyclerviewtemplate.drawer_menu.DrawerBodyItem
-import jp.androidbook.recyclerviewtemplate.drawer_menu.DrawerHeaderItem
-import jp.androidbook.recyclerviewtemplate.drawer_menu.DrawerListData
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -32,46 +23,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolBar)
-        setupDrawerRecyclerView()
         setupNavigation()
-    }
-
-    private fun setupDrawerRecyclerView() {
-        val groupAdapter = GroupAdapter<GroupieViewHolder>().apply {
-            setOnItemClickListener { item, _ ->
-                when (item.id) {
-                    DrawerListData.DrawerBodyType.SimpleList.id -> {
-                        navController.navigate(R.id.simpleRecyclerFragment)
-                    }
-                    DrawerListData.DrawerBodyType.GridTwo.id -> {
-                        navController.navigate(R.id.gridSelectionFragment)
-                    }
-                    DrawerListData.DrawerBodyType.GridThree.id -> {
-                        navController.navigate(R.id.gridSelectionFragment)
-                    }
-                    DrawerListData.DrawerBodyType.GridStaggered.id -> {
-                        navController.navigate(R.id.staggeredGridFragment)
-                    }
-                    DrawerListData.DrawerBodyType.UpdatableList.id -> {
-                        navController.navigate(R.id.updateFragment)
-                    }
-                }
-            }
-        }
-        findViewById<RecyclerView>(R.id.drawerContentRecyclerView).apply {
-            layoutManager = LinearLayoutManager(this@MainActivity).apply {
-                orientation = RecyclerView.VERTICAL
-            }
-            addItemDecoration(DividerItemDecoration(this@MainActivity, RecyclerView.VERTICAL))
-            adapter = groupAdapter
-        }
-        val items = mutableListOf<Item>(
-            DrawerHeaderItem()
-        )
-        for (drawerBodyType: DrawerListData.DrawerBodyType in DrawerListData.getDrawerBodyList()) {
-            items.add(DrawerBodyItem(drawerBodyType.id, drawerBodyType.drawerBodyText))
-        }
-        groupAdapter.update(items)
     }
 
     private fun setupNavigation() {
