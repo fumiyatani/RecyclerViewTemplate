8b166635fa7533c09f743ea7b8289ca31b61c597
tanifumiya <fumiyadeveloper@gmail.com>
2021-02-21 02:35:11 +0900

    Hilt追加

---
+++ app/src/main/java/jp/androidbook/recyclerviewtemplate/di/SimpleListModule.kt
@@ -0,0 +1,9 @@
+package jp.androidbook.recyclerviewtemplate.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+
+@Module
+@InstallIn(FragmentComponent::class)
+object SimpleListModule
