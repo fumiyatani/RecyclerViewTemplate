fa8e0236f006b45cf85ff5b5339163e6b1d8efea
tanifumiya <fumiyadeveloper@gmail.com>
2022-10-07 23:02:08 +0900

    add Log.d for Fragment lifecycle

--- app/src/main/java/jp/androidbook/recyclerviewtemplate/grid/three/ThreeGridFragment.kt
+++ app/src/main/java/jp/androidbook/recyclerviewtemplate/grid/three/ThreeGridFragment.kt
@@ -2,6 +2,7 @@ package jp.androidbook.recyclerviewtemplate.grid.three
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,11 +19,19 @@ class ThreeGridFragment : Fragment(), OnTappedRecyclerViewListener {
 
     private lateinit var binding: FragmentThreeGridBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate: $CLASS_NAME")
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d(TAG, "onCreateView: $CLASS_NAME")
+
         binding = FragmentThreeGridBinding.inflate(inflater, container, false)
 
         return binding.root
@@ -31,7 +40,33 @@ class ThreeGridFragment : Fragment(), OnTappedRecyclerViewListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        Log.d(TAG, "onViewCreated: $CLASS_NAME")
+
         setupRecyclerView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "★★onResume★★: $CLASS_NAME")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, "★★onPause★★: $CLASS_NAME")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "onStop: $CLASS_NAME")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy: $CLASS_NAME")
     }
 
     private fun setupRecyclerView() {
@@ -48,5 +83,10 @@ class ThreeGridFragment : Fragment(), OnTappedRecyclerViewListener {
 
     override fun onTapped(text: String) {
         Toast.makeText(context, "${text}です。", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val TAG = "DEBUGDEBUG"
+        private const val CLASS_NAME = "ThreeGridFragment"
     }
 }
